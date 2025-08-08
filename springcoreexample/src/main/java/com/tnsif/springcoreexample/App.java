fbe7eeee32ceb254336748264d159bd5423400c9
Thv0529 <thamizharasi415@gmail.com>
2025-08-08 19:46:03 +0530

    SpringCoreExample

---
+++ springcoreexample/src/main/java/com/tnsif/springcoreexample/App.java
@@ -0,0 +1,18 @@
+package com.tnsif.springcoreexample;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+  public static void main(String[] args) {
+	  
+	  //It is the IOC container
+	  //It reads and manages the beans
+	  
+	  ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+	  
+	  College college = (College)context.getBean("collegeBean");
+	  
+	  college.show();
+  }
+}
